a1f47640cf5146701293381b619f05a0072035e1
RAGHVENDRA YADAV <ragh.yadav2408@gmail.com>
2019-10-13 22:04:36 +0530

    Updated

--- src/com/ils/genericmethods/Generic_Methods.java
+++ src/com/ils/genericmethods/Generic_Methods.java
@@ -11,15 +11,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-
-public class Generic_Methods  {
+public class Generic_Methods {
 
 	public static WebDriver driver;
 
-
 	public static WebElement getelement(String elementname) throws IOException {
 
-				String[] arr = elementname.split("#");
+		String[] arr = elementname.split("#");
 		WebElement we = null;
 		if (arr[0].equalsIgnoreCase("name") == true) {
 			we = driver.findElement(By.name(arr[1]));
@@ -28,20 +26,18 @@ public class Generic_Methods  {
 
 			we = driver.findElement(By.linkText(arr[1]));
 		} else if (arr[0].equalsIgnoreCase("xpath")) {
-			System.out.println("Xpath Matched...");
+
 			we = driver.findElement(By.xpath(arr[1]));
 		}
-
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 		return we;
 	}
-
 
 	public static void click(String elementname) throws IOException {
 		WebElement we = getelement(elementname);
 		we.click();
 	}
-
-
 
 	public static void openapp(String brname, String url) {
 
@@ -50,8 +46,7 @@ public class Generic_Methods  {
 
 		} else if (brname.equalsIgnoreCase("CH") == true) {
 
-			System.setProperty("webdriver.chrome.driver",
-					"C:\\ProgramData\\Chrome_driver_76.0.3809.68\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "\\driver\\chromedriver.exe");
 			driver = new ChromeDriver();
 		} else if (brname.equalsIgnoreCase("IE") == true) {
 			System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
@@ -62,32 +57,31 @@ public class Generic_Methods  {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
-	
+
 	public static WebDriver driver(String brname) {
+		String current_path = System.getProperty("user.dir");
 
 		if (brname.equalsIgnoreCase("firefox") == true) {
 			driver = new FirefoxDriver();
 
 		} else if (brname.equalsIgnoreCase("Chrome") == true) {
 
-			System.setProperty("webdriver.chrome.driver",
-					"C:\\ProgramData\\Chrome_driver_76.0.3809.68\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", current_path + "\\driver\\chromedriver.exe");
 			driver = new ChromeDriver();
 		} else if (brname.equalsIgnoreCase("IE") == true) {
 			System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
 		}
 
-		return driver;		
+		return driver;
 	}
 
 	public static void url(String url) {
-		
+
 		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();		
-		
+		// driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// driver.manage().window().maximize();
+
 	}
-	
 
 }
